4226584430931773e45fc8aabc96b5eabab2b873
blackdahila <ola.zxcvbnm@gmail.com>
2018-08-11 15:14:00 +0200

    Add score text.

---
+++ LD42/Assets/Scripts/Eatable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eatable : MonoBehaviour 
+{
+	public int Value;
+	
+	public void GetEaten() {
+		Debug.Log("Eaten for " + Value);
+	}
+}
