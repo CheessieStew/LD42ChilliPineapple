cf277539a8ade6d94225cb150d15ecb972c09472
CheessieStew <serindrack@gmail.com>
2018-08-11 19:39:44 +0200

    stopping on collisions

--- LD42/Assets/Scripts/DragonSpeed.cs
+++ LD42/Assets/Scripts/DragonSpeed.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class DragonSpeed : MonoBehaviour 
 {
-	public float Speed;
+	public float Accelleration;
+	public float Decceleration;
+	public float TopSpeed;
+	public float Speed {get; private set;}
+	private bool _collided;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+	
+	/// <summary>
+	/// OnCollisionStay is called once per frame for every collider/rigidbody
+	/// that is touching rigidbody/collider.
+	/// </summary>
+	/// <param name="other">The Collision data associated with this collision.</param>
+	void OnCollisionStay(Collision other)
+	{
+		_collided = true;
+	}
+
 	// Update is called once per frame
 	void Update () {
+		if (_collided)
+		{
+			Speed = Mathf.Max(0,Speed -  Decceleration*Time.deltaTime);
+		}
+		else
+		{
+			Speed = Mathf.Min(TopSpeed, Speed + Accelleration*Time.deltaTime);
+		}		
 		transform.position += Vector3.right * Speed * Time.deltaTime;
+		_collided = false;
 	}
 }
