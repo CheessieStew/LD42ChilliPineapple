3c780483ff235ffa3b3624bdee0abe83f362bae5
hasparus <hasparus@gmail.com>
2018-08-11 15:23:02 +0200

    Add Shooter.

---
+++ LD42/Assets/Prefabs/Archer/Shooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+	public float rotationSpeed = 0.8f;
+
+	Transform bow;
+	Transform target;
+
+	void Start()
+	{
+		bow = transform.Find("Bow");
+		target = GameObject.FindWithTag("Player").transform;
+	}
+
+	void Update()
+	{
+		bow.localPosition = Vector3.RotateTowards(
+			bow.localPosition,
+			target.position - transform.position,
+			rotationSpeed * Time.deltaTime,
+			0f);
+	}
+}
