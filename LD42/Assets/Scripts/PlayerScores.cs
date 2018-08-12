9934b307987d5e26e3ffec1c58ecf30b00fa2de5
hasparus <hasparus@gmail.com>
2018-08-12 20:56:23 +0200

    Fix tower prefabs and Eating script.

--- LD42/Assets/Scripts/PlayerScores.cs
+++ LD42/Assets/Scripts/PlayerScores.cs
@@ -11,35 +11,36 @@ public class PlayerScores : MonoBehaviour {
     public InputField nameInputField;
 
     private ScoresController scoresCtrl;
-    private int score;
-    private string name;
+    private int _score;
+    public int Score {
+        get { return _score; }
+        set {
+            _score = value;
+            SetCountText();
+        }
+    }
     private bool showTopScores;
 
     void Start() {
         scoresCtrl = gameObject.GetComponent<ScoresController>();
         Debug.Assert(scoresCtrl != null, "Scores Controller must be assigned.");
-        name = "";
-        score = 0;
-        SetCountText();
+        Score = 0;
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
             print("E key was pressed");
-            if (showTopScores) HideTopScores(); 
+            if (showTopScores) HideTopScores();
             else ShowTopScores();
         }
     }
 
     public void ShowTopScores() {
-        try 
-        {
+        try {
             var scores = scoresCtrl.GetUserScores().ToString();
-			topScores.text = scoresCtrl.GetUserScores().ToString();      
-        }
-        catch (System.Exception ex)
-        {
+            topScores.text = scoresCtrl.GetUserScores().ToString();
+        } catch (System.Exception ex) {
             Debug.Log(ex);
         }
     }
@@ -49,18 +50,13 @@ public class PlayerScores : MonoBehaviour {
     }
 
     public void SendUserScore() {
-		var userScore = new UserScore(nameInputField.text, score);
-		scoresCtrl.CreateOrUpdateUserScore(userScore);
-    }
-
-    public void UpdateCurrentScore() {
-        score++;
-        SetCountText();
+        var userScore = new UserScore(nameInputField.text, Score);
+        scoresCtrl.CreateOrUpdateUserScore(userScore);
     }
 
     void SetCountText() {
         if (currentScoreText == null)
             return;
-        currentScoreText.text = "Score: " + score.ToString();
+        currentScoreText.text = $"Score: {Score}";
     }
 }
